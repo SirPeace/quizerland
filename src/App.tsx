9997418867ee7987bb77edf39b1fb29588f988a7
hubra <hubra@hismith.ru>
2023-02-23 13:59:30 +0300

    Init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+    <div>
+      <header>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <span>
           <span>Learn </span>
           <a
-            className="App-link"
             href="https://reactjs.org/"
             target="_blank"
             rel="noopener noreferrer"
@@ -24,7 +16,6 @@ function App() {
           </a>
           <span>, </span>
           <a
-            className="App-link"
             href="https://redux.js.org/"
             target="_blank"
             rel="noopener noreferrer"
@@ -33,7 +24,6 @@ function App() {
           </a>
           <span>, </span>
           <a
-            className="App-link"
             href="https://redux-toolkit.js.org/"
             target="_blank"
             rel="noopener noreferrer"
@@ -42,7 +32,6 @@ function App() {
           </a>
           ,<span> and </span>
           <a
-            className="App-link"
             href="https://react-redux.js.org/"
             target="_blank"
             rel="noopener noreferrer"
@@ -52,7 +41,7 @@ function App() {
         </span>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
